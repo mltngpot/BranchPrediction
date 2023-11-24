7c3e6839fb142611066412eec0ffb5a50910409d
Travis Thurn <travis.thurn@fiserv.com>
2023-11-24 17:07:22 -0600

    added trace file

--- BranchPrediction/src/BranchPrediction.cpp
+++ BranchPrediction/src/BranchPrediction.cpp
@@ -2,6 +2,7 @@
 #include "include/Buffer.h"
 #include "include/BufferEntry.h"
 #include "include/PatternHistoryTable.h"
+#include "include/PredictStats.h"
 
 using namespace std;
 
@@ -11,11 +12,16 @@ int main()
 {
 	Buffer* buffer = new Buffer(cin);
 	PatternHistoryTable* pht = new PatternHistoryTable(PHT_HISTORY_SIZE);
-	
+	PredictStats stats = PredictStats();
+
 	while (!buffer->isEOF())
 	{
 		BufferEntry* entry = buffer->pop();
+		bool prediction = pht->predict(entry->address);
+		stats.update(prediction, entry->taken);
 		pht->update(*entry);
 	};
+
+	stats.print();
 
 }
